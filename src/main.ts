d4301d473b46071891dd04a745d10bf28fbb7244
Th3B3n1 <othersc9bst@gmail.com>
2024-01-31 13:44:25 +0100

    4) - Added the ability to get the smallest number of the list

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
-let list : Array<Number> = [];
+let list : Array<number> = [];
 let numberInput : HTMLInputElement = document.getElementById("numberInput") as HTMLInputElement;
 let numberAppendButton : HTMLButtonElement = document.getElementById("numberAppend") as HTMLButtonElement;
 let results : HTMLDivElement = document.getElementById("results") as HTMLDivElement;
+let minimum : HTMLParagraphElement = document.getElementById("minimum") as HTMLParagraphElement;
+let getMinimumButton : HTMLButtonElement = document.getElementById("getMinimum") as HTMLButtonElement;
 
 numberAppendButton.addEventListener("click", 
 function() 
@@ -19,7 +21,27 @@ function()
   }
 })
 
-function AddNumberToList(number : number, list : Array<Number>) : void
+getMinimumButton.addEventListener("click", 
+function()
+{
+  let min : number = GetMinimum(list);
+  minimum.innerHTML = min.toString();
+})
+
+function AddNumberToList(number : number, list : Array<number>) : void
 {
   list.push(number);
+}
+
+function GetMinimum(list : Array<number>) : number
+{
+  let min : number = list[0];
+  for (let i : number = 1; i < list.length; i++)
+  {
+    if (list[i] < min)
+    {
+      min = list[i];
+    }
+  }
+  return min;
 }
